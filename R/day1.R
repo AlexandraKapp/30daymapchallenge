e8a154a98a0b6ecf44e5eae38abfdaaca7a19226
AlexandraKapp <alexandra.kapp2@gmail.com>
2020-11-01 10:33:10 +0100

    change from leaflet to mapdeck

--- R/day1.R
+++ R/day1.R
@@ -1,42 +1,63 @@
 ### Day 1: Points
 
 library(sf)
-library(leaflet)
 library(dplyr)
-library(htmlwidgets)
-library(htmltools)
+library(mapdeck)
+
+mapboxAccessToken <- "TOKEN"
 
 # prepare streets data
-blocks <- st_read("data/bloecke.shp") %>% 
+blocks <- st_read("data/bloecke.shp") %>%
   st_make_valid() # some blocks are not a valid polygon
 
-xhain <- st_read("data/berlin_bz.geojson") %>% 
+xhain <- st_read("data/berlin_bz.geojson") %>%
   filter(name == "Friedrichshain-Kreuzberg")
 
-blocks_in_xhain <- st_intersection(blocks, st_transform(xhain$geometry, 25833)) %>% 
-  st_combine() %>% 
+blocks_in_xhain <- st_intersection(blocks, st_transform(xhain$geometry, 25833)) %>%
+  st_combine() %>%
   st_make_valid()
 
 streets_xhain <- st_difference(st_transform(xhain$geometry, 25833), blocks_in_xhain)
 
-#### create cars as points 
+#### create cars as points
 # amount of cars in XHain 80808
 # https://s3.kleine-anfragen.de/ka-prod/be/18/20848.pdf
 
 amount_cars_xhain <- 80808
 
 # this takes about a minute
-cars <- st_sample(streets_xhain,size=amount_cars_xhain) %>% 
-  st_transform(4326)
+cars <- st_sample(streets_xhain, size = amount_cars_xhain) %>%
+  st_transform(4326) %>%
+  st_as_sf()
 
-map <- leaflet(cars) %>% 
-  addProviderTiles(providers$CartoDB.DarkMatter, options = providerTileOptions(minZoom = 14, maxZoom = 15) ) %>%
-  setView(13.43, 52.51, 14) %>% 
-  addCircleMarkers(radius = 1, fillColor= "white", color = "transparent") %>% 
-  addControl(html = paste(tags$div(HTML("<a href = '../index.html'>< Back to overview</a>")),
-    tags$h2(HTML("All 80.808 cars registered in XHain mapped onto the streets.")),
-                          tags$div(HTML("Data: <a href= https://fbinter.stadt-berlin.de/fb/index.jsp?loginkey=zoomStart&mapId=ISU5@senstadt&bbox=387452,5818178,395140,5822472'>Geopartal Berlin [Blockkarte 1:5000 (ISU5)]</a> & 
-                                        <a href='https://s3.kleine-anfragen.de/ka-prod/be/18/20848.pdf'>kleine Anfragen</a>"))),
-             position = "bottomleft")
+cars <- cars %>%
+  mutate(
+    lon = st_coordinates(cars)[, 1],
+    lat = st_coordinates(cars)[, 2]
+  ) %>%
+  st_drop_geometry()
 
-saveWidget(map, file="day1.html")
+map <- mapdeck(
+  style = mapdeck_style("dark"),
+  location = c(13.43, 52.51, 14),
+  zoom = 13,
+  min_zoom = 13,
+  max_zoom = 15,
+  token = mapboxAccessToken
+) %>%
+  add_scatterplot(
+    data = cars,
+    lon = "lon",
+    lat = "lat",
+    fill_colour = "#FFFFFF",
+    stroke_colour = "#FFFFFF",
+    radius = 2,
+    update_view = F
+  ) %>%
+  add_title(title = list(
+    title = "<b>All 80.808 cars registered in XHain mapped onto the streets</b> <br> <span style='font-size:14px'>Data: <a href= https://fbinter.stadt-berlin.de/fb/index.jsp?loginkey=zoomStart&mapId=ISU5@senstadt&bbox=387452,5818178,395140,5822472'>Geopartal Berlin [Blockkarte 1:5000 (ISU5)]</a> & 
+                                        <a href='https://s3.kleine-anfragen.de/ka-prod/be/18/20848.pdf'>kleine Anfragen</a> <br> <a href = '../index.html'>< Back to overview</a> </span>",
+    css = "font-size: 20px;"
+  ))
+
+mapshot(map, "html/day1.html")
