f35bc6e5f7a2b20b68449a6c0ed450dd11086050
AlexandraKapp <alexandra.kapp2@gmail.com>
2020-11-03 09:13:03 +0100

    update hover label

--- R/day3.R
+++ R/day3.R
@@ -14,18 +14,21 @@ e <- st_bbox(berlin)
 boulder_gyms <- c("Cliffhanger Boulderlounge Berlin", "Basement Boulderstudio Berlin","Ostbloc Berlin","Boulderworkx Berlin", "Boulderklub Berlin",
                   "Bright Side Berlin","Der Kegel Berlin","Südbloc Berlin","Berta Block Berlin","Bouldergarten Berlin")
 
+# gecode gyms and manually fix the ones that are not found
 gyms_geo <- geocode_OSM(boulder_gyms) %>% 
   dplyr::select(query, lat, lon) %>% 
   rbind(c("Boulderworkx Berlin", 52.486775, 13.318185)) %>% 
   rbind(c("Brigth Side Berlin", 52.481917, 13.365807)) %>% 
   rbind(c("Cliffhanger Boulderlounge Berlin", 52.542148, 13.220973))
 
+# create voronoi map
 vor <- voronoi(gyms_geo[c("lon", "lat")], ext = c(e$xmin, e$xmax, e$ymin, e$ymax)) %>% 
-  st_as_sf()
+  st_as_sf() %>% 
+  cbind(gyms_geo$query)
 st_crs(vor) <- 4326
 
-intersection_with_berlin <- st_intersection(vor, berlin$geometry)
-m <- mapview(intersection_with_berlin, legend = F, layer.name = "area", popup = F) + 
+intersection_with_berlin <- st_intersection(vor, berlin$geometry) %>% dplyr::select(gyms_geo.query)
+m <- mapview(intersection_with_berlin, legend = F, layer.name = "area", popup = F ) + 
   mapview(st_as_sf(gyms_geo, coords = c("lon", "lat"), crs = 4326), legend = F, layer.name = "Boulder gyms", popup = F)
 
 mapshot(m, "day3.html")
